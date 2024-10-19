ad12965e90be2f6a71136b0223653d9d487640ef
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-10-19 19:14:11 +0800

    a

--- Course2-React/Content_Rating/contentrating/src/App.js
+++ Course2-React/Content_Rating/contentrating/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      like: 0,
+      dislike: 0,
+      handleLike: () => {
+        alert("Liked");
+        this.setState((prevState) => ({like: prevState.like + 1}));
+        return;
+      },
+  
+      handleDislike: () => {
+        alert("Disliked");
+        this.setState((prevState) => ({dislike: prevState.dislike + 1}));
+        return;
+      }
+    };
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Do you like this content?</h1>
+        <button onClick={this.state.handleLike}>
+          Like: {this.state.like}
+        </button>
+        <button onClick={this.state.handleDislike}>
+          Dislike: {this.state.dislike}
+        </button>
+      </div>
+    );
+  }
+
 }
 
 export default App;

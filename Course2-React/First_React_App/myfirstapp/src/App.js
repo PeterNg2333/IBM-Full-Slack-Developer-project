fc688dbb53b13a46055d259ffa816fa5076fd304
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-10-19 18:36:30 +0800

    W 1

--- Course2-React/First_React_App/myfirstapp/src/App.js
+++ Course2-React/First_React_App/myfirstapp/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const currentDate = new Date();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div>
+        <h1>Module 1.1 Hello World</h1>
+        <h2>Now is {currentDate.toLocaleTimeString()}</h2>
+        <hr />
+      </div>
+    </section>
   );
 }
 

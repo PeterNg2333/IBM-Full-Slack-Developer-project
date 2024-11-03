691778fc84c76100e1b2db774035fcafaaa3b717
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-11-04 01:35:09 +0800

    past timer

--- temp.js
+++ temp.js
@@ -245,3 +245,178 @@
         
         console.log("Script initialized. Press 'A' to start automation.");
     })();
+
+(function(){
+    /* 
+    * Override console.log to capture messages 
+    * This unit will capture all console.log messages and store them in an array. 
+    * And it will only log the message if it is different from the previous message or more than 1 second has passed.
+    */
+    const consoleMessages = [];
+    const originalConsoleLog = console.log;
+    console.log = function(...args) {
+        let curContent = args.join(' ');
+        let curTime = Date.now()/1000;
+        let previousMessage = consoleMessages[consoleMessages.length-1];
+        let prevContent = previousMessage ? previousMessage.content : '';
+        let prevTime = previousMessage ? previousMessage.time : 0;
+        let timeDiff = curTime - prevTime;
+        if (curContent !== prevContent || timeDiff > 1) { // Only log if different message or more than 1 second has passed
+            consoleMessages.push({content, time});
+        } 
+        console.warn(`Updated consoleMessages: ${JSON.stringify(consoleMessages)}`);
+        originalConsoleLog.apply(console, args);
+    };
+
+    /* Global Variables */
+    let QUESTION_LIST = ['我想上廁所，廁所喺邊度？', '我想急尿？', '我要喂奶，育嬰室喺邊度？', '育嬰間喺邊度？', '邊度有殘廁？']
+    let ANSWER_LIST = [];
+    let QUESTION_COUNTER = 0;
+    let START_TIME = null;
+    let PREVIOUS_RESPONSE = '';
+    let CURRENT_QUESTION = '';
+    let WAITING_TIME = 5000; // 5 seconds
+    let INPUT_BOX = document.querySelector("#form .sc-textarea");
+    let SEND_BUTTON = document.querySelector("#form .sc-user-input--send-icon-wrapper");
+    const INPUT_EVENT = new Event('input', { bubbles: true });
+    if (!inputBox || !sendButton) {
+        console.error("Input box or send button or message window not found.");return}
+
+    if (QUESTION_LIST.length === 0) {
+        let questions = prompt("Please enter the questions separated by comma: ");
+        try{
+            if (questions && questions.length > 0) QUESTION_LIST = questions.split(',').map(q => q.trim());}
+        catch(err) {console.error("Error parsing questions. Exiting...", err);}
+    }
+
+    /* add Event Listener to listen for keydown events */
+    document.body.addEventListener('keydown', () => {
+        let eventKey = event.key.toLowerCase();
+        if (eventKey === 'a') {
+            startAutomation();
+        }
+    })
+
+    /* 
+    * Function to start the automation 
+    * In the first round, it will send the first question from the list
+    * In the subsequent rounds, it will called by the endTimer function to send the next question until all questions are answered
+    * This is recursive function that will call itself until all questions are answered
+    */
+    startAutomation = () => {
+        // Check if all questions have been answered
+        let allQuestionsAnswered = QUESTION_COUNTER >= QUESTION_LIST.length;
+        if (allQuestionsAnswered){
+        // If yes, 
+            // Cpy all responses to clipboard and return. 
+            // But if it is failed to copy due to user lost focus, it copy a csv string a alert user to copy manually
+            let allResponses = ANSWER_LIST.join('\n');
+            navigator.clipboard.writeText(allResponses)
+                .then(() => {console.warn("All responses copied to clipboard!");})
+                .catch(err => {
+                    console.error('Failed to copy responses to clipboard. Please copy the following csv string manually:', allResponses);
+                    alert(allResponses);
+                });
+        }else {
+        // If no, 
+            // proceed to send the next question by sending the question and starting the timer
+            CURRENT_QUESTION = QUESTION_LIST[QUESTION_COUNTER];
+            INPUT_BOX.value = CURRENT_QUESTION; 
+            INPUT_BOX.dispatchEvent(INPUT_EVENT); // Dispatch input event
+            SEND_BUTTON.click(); 
+            console.warn(`Sent message: ${CURRENT_QUESTION}`);
+            INPUT_BOX.value = ''; // Clear the input box
+            QUESTION_COUNTER++;
+            startTimer(); 
+        }
+        return;
+    }
+
+    /*
+    * Function to start the timer
+    * It will start the timer by setting the START_TIME to the current time
+    * And it will track the console messages and screen updates using MutationObserver
+    */
+    startTimer = () => {
+        // Start the timer
+        START_TIME = Date.now(); 
+        console.warn(`Start Timer at ${Date()}`);
+
+        // Track console messages and screen updates by checking the message window using MutationObserver
+        let messageWindow = document.querySelector("#messageWindow");
+        if (!messageWindow) {console.error("Message window not found. Exiting...");return;}
+        let observer = new MutationObserver(() => {
+            endTimer();
+        });
+        observer.observe(messageWindow, {childList: true, subtree: true});
+        console.warn(`Observer started... at ${Date()}`);
+    }
+
+    /*
+    * Function to end the timer
+    * It will called by the MutationObserver when the screen is updated
+    * IF the following conditions are met:
+    *     1. The last sent message node is equal to the current question
+    *     2. The last received message node is not equal to the previous response or the time difference is more than 1 second
+    * Then it will perform the following actions:
+    *    1. Stop the timer and calculate the elapsed time
+    *    2. Write the response to the Answer List
+    *    3. Copy the response and time to clipboard 
+    *    4. Assign the last received message to the PREVIOUS_RESPONSE
+    *    5. Stop the observer and Wait for 5 seconds before starting the next round
+    *     
+    */
+    endTimer = () => {
+        console.warn(`Try to end the timer at ${Date()}`);
+        // Conditions to check 
+        let endTime= Date.now()/1000;
+        let allMsgReceived = document.querySelectorAll(".timestamp.timestamp-received");
+        let lastNodeReceived = allMsgReceived[allMsgReceived.length - 1];
+        let allMsgSent = document.querySelectorAll(".timestamp.timestamp-sent");
+        let lastNodeSent = allMsgSent[allMsgSent.length - 1];
+        let lastReceivedMsg = lastNodeReceived ? lastNodeReceived.innerText : '';
+        let lastSentMsg = lastNodeSent ? lastNodeSent.innerText : '';
+        let lastSentMsgEqualCurrentQuestion = lastSentMsg === CURRENT_QUESTION;
+        for (let i = consoleMessages.length - 1; i >= 0; i--) {
+            let consoleMsg = consoleMessages[i];
+            let consoleMsgContent = consoleMsg.content;
+            let consoleMsgTime = consoleMsg.time;
+            let timeDiff = endTime - consoleMsgTime;
+            if (PREVIOUS_RESPONSE !== consoleMsgContent && timeDiff > 1) {
+                lastReceivedMsgNotEqualPreviousResponse = true;
+                break;
+            }else {
+                lastReceivedMsgNotEqualPreviousResponse = false;
+            }
+        }
+
+        if (lastSentMsgEqualCurrentQuestion && lastReceivedMsgNotEqualPreviousResponse) {
+            // Stop the timer and calculate the elapsed time
+            let timeElapsed = (endTime - START_TIME) / 1000; // in seconds
+            console.warn(`End Timer. Time interval: ${timeElapsed} seconds`);
+
+            // Copy response and time to clipboard
+            let responseEntry = `${lastReceivedMsg}\t${timeElapsed}`;
+            ANSWER_LIST.push(responseEntry);
+            navigator.clipboard.writeText(responseEntry)
+                .then(() => {console.warn("Response copied to clipboard!");})
+                .catch(err => {console.error('Failed to copy response to clipboard. Error:', err);});
+
+            // Assign the last received message to the PREVIOUS_RESPONSE
+            PREVIOUS_RESPONSE = lastReceivedMsg;
+
+            // Stop the observer and Wait for 5 seconds before starting the next round
+            let messageWindow = document.querySelector("#messageWindow");
+            let observer = messageWindow._observer;
+            observer.disconnect();
+            console.warn(`Observer stopped at ${Date()}`);
+            setTimeout(() => {startAutomation();}, WAITING_TIME);
+        }
+        return
+    }
+
+})()
+
+
+
+
